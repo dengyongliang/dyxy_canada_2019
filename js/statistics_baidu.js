b2c263dcd1acbe8984098b0cdc32109a72bebffa
No-Problem <dyliang1985@hotmail.com>
2019-05-09 21:43:33 +0800

    20190509

---
+++ js/statistics_baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ff43a237b95e20879c3d09db853a3722";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
